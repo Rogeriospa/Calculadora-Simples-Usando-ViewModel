826c07ec7efd056e53503f59300918e53b94268d
Rogério da Silva Souza <80793906+Rogeriospa@users.noreply.github.com>
2021-05-19 09:49:55 -0300

    Create MultableLiveData.kt
    Criar essa class dentro da pasta model:
    
    package com.rogerio.calculadorasimples.model

---
+++ MultableLiveData.kt
@@ -0,0 +1,15 @@
+package com.rogerio.calculadorasimples.model
+
+import androidx.lifecycle.Observer
+import com.rogerio.calculadorasimples.MainActivity
+
+class MultableLiveData<T> {
+    lateinit var value: CalcVisor
+
+    fun observe(mainActivity: MainActivity, observer: Observer<CalcVisor>) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+    var value: T = TODO()
